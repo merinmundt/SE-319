a5cd8cc3054dd581203b943fd601228d024a10a8
Merin Mundt <merinmundt@Merins-MacBook-Pro.local>
2018-09-18 22:14:55 -0500

    going to debug

--- homework2/validation1.js
+++ homework2/validation1.js
@@ -1,33 +1,83 @@
-function alphanumeric(inputtext){
-    var bool = false;
+function alphanumericf(inputtext){
+    var isValid = false;
     var letternumber = /^[0-9a-zA-Z]+$/;
     if(inputtext.value.match(letternumber)){
-        bool = true;
+        isValid = true;
     }
-    return bool;
+    if(isValid){
+        unhideCheck();
+    }
+    else{
+        unhideX();
+    }
+    return isValid;
 }
-function dropdown(){}
-{
-    var e = document.getElementById("ddlView");
-    var strUser = e.options[e.selectedIndex].value;
 
-    var strUser1 = e.options[e.selectedIndex].text;
+function alphanumericl(inputtext){
+    var isValid = false;
+    var letternumber = /^[0-9a-zA-Z]+$/;
+    if(inputtext.value.match(letternumber)){
+        isValid = true;
+    }
+    if(isValid){
+        unhideCheck();
+    }
+    else{
+        unhideX();
+    }
+    return isValid;
+}
+
+function dropdowng(inputtext){}
+{
+    var strUser = inputtext.options[inputtext.selectedIndex].value;
+
+    var strUser1 = inputtext.options[inputtext.selectedIndex].text;
     if(strUser==0)
     {
-        alert("Please select a user");
+        unhideX();
+    }
+    else{
+        unhideCheck();
     }
 }
+
+function dropdowns(inputtext){}
+{
+    var strUser = inputtext.options[inputtext.selectedIndex].value;
+
+    var strUser1 = inputtext.options[inputtext.selectedIndex].text;
+    if(strUser==0)
+    {
+            unhideX();
+    }
+    else{
+        unhideCheck();
+    }
+}
+
+function unhideX() {
+    var img = Document.getElementById("Ximage");
+    img.style.visibility = 'visible';
+}
+
+function unhideCheck(){
+    var img = Document.getElementById("Checkimage");
+    img.style.visibility = 'visible';
+    
+}
+
 function validate(){
-    if(first name){
-        alphanumeric(firstname);
-    }
-    if(last name){
-        alphanumeric(lastname);
-    }
-    if(gender){
-        dropdown();
-    }
-    if(state){
-        dropdown();
-    }
+    var firstname = document.getElementById("firstname");
+        alphanumericf(firstname.value);
+    
+    var lastname = document.getElementById("lastname");
+        alphanumericl(lastname.value);
+    
+    var gender = document.getElementById("gender");
+        dropdowng(gender.value);
+    
+    var state = document.getElementById("state");
+        dropdowns(state.value);
+    
 }
--- homework2/validation2.js
+++ homework2/validation2.js
@@ -1,34 +1,52 @@
 function checkEmail(email){
     var pattern = /^[a-zA-Z0-9]{3}+@[a-zA-Z0-9]{3}+\.[a-z]{3}$/;
+    if(pattern.test(email)){
+        unhideCheck();
+    }
+    else{
+        unhideX();
+    }
     return pattern.test(email);
 }
 
 function checkNum(inputtext){
-    var bool = false;
+    var isValid = false;
     var Number = /^\(?([0-9]{3})\)?[-]?([0-9]{3})[-]?([0-9]{4})$/;
     if(inputtext.value.match(Number)){
-        bool = true;
+        isValid = true;
     }
-    return bool;
+    if(isValid){
+        unhideCheck();
+    }
+    else{
+        unhideX();
+    }
+    return isValid;
 }
 
 function checkAddress(inputtext){
-    var bool = false;
+    var isValid = false;
     var order = /^[A-Za-z ]+(?:,?\s+)[A-Za-z]{2,}$/;
     if(inputtext.test(order)){
-        bool = true;
+        isValid = true;
     }
-    return bool;
+    if(isValid){
+        unhideCheck();
+    }
+    else{
+        unhideX();
+    }
+    return isValid;
 }
 
 function Validate(){
-    if(email){
-        checkEmail();
-    }
-    if(phonenumber)[
-        checkNum();
-    ]
-    if(address){
-        checkAddress();
-    }
+    var email = document.getElementById("email");
+        checkEmail(email.value);
+    
+    var number = document.getElementById("num");
+        checkNum(number.value);
+    
+    var address = document.getElementById("add");
+        checkAddress(address.value);
+    
 }
