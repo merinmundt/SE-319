040e6a0381aac35494d91b1c519f0e2dc7a51f89
Merin Mundt <merinmundt@Merins-MacBook-Pro.local>
2018-09-28 21:35:37 -0500

    wip

--- Homework3/calculator.js
+++ Homework3/calculator.js
@@ -1,3 +1,49 @@
-<html>
+//listen to the keys being pressed
+//determine which key is being pressed
+var memory;
+var oldmemory;
+var operator;
+var button;
+var oldbutton;
 
-</html>
+function handleButton(button){
+    
+    var action = button.dataset.action;
+
+    switch(action){
+        case "number":
+            handleNumber(button.value);
+
+        case "add":
+
+            operator = "add"; 
+            break;
+
+        case "subtract":
+            operator = "add";
+
+        case "multiply":
+            operator = "add";
+
+        case "divide":
+            operator = "add";
+
+        case "clear":
+        
+        case "memorysub":
+
+        case "memoryadd":
+
+        case "memoryclear":
+
+    }
+
+
+}
+
+function handleNumber(number){
+        var answer = document.getElementById("ans");
+        answer.value = answer.value + number;
+
+}
+
