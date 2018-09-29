bd0868a82b5336986c573c8f1d55667598000e3e
Merin Mundt <merinmundt@Merins-MBP.local.tld>
2018-09-29 16:59:26 -0500

    snake WIP

--- Homework3/snake.js
+++ Homework3/snake.js
@@ -4,16 +4,48 @@ ctx.beginPath();
 ctx.moveTo(0,300);
 var posX = 0;
 var posY = 300;
+var timer = null;
+var direction = "right";//right, left, up, down
 
 function startStop(){
-    draw();
+    var button = document.getElementById("startStop");
+    if(!timer){
+        timer = setInterval(function(){
+            draw();
+        },1000);
+        button.innerHTML = "Stop";
+    }
+    else{
+        clearInterval(timer);
+        timer = null;
+        button.innerHTML = "Start";
+    }
 }
 function turnLeft(){
+    switch(direction){
+        case "right":
 
+        case "left":
+
+        case "up":
+
+        case "down":
+
+    }
 }
 
-function turnRight(){
 
+function turnRight(){
+    switch(direction){
+        case "right":
+
+        case "left":
+
+        case "up":
+
+        case "down":
+    
+    }
 }
 
 function draw(){
