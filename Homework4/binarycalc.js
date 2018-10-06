695ae48a71c63da3b7ee96c93e2dac514fbb4ef6
Merin Mundt <merinmundt@Merins-MacBook-Pro.local>
2018-10-06 15:08:51 -0500

    wip

--- Homework4/binarycalc.js
+++ Homework4/binarycalc.js
@@ -1,22 +1,4 @@
 var rs = require('readline-sync');
-main();
-//var result = fNum1 + action + fNum2
-//var result = eval(result);
-//console.log(result);
-
-function main(){
-    var fNum1 = rs.question('1st Number: ');
-    var fNum2 = rs.question('2nd Number: ');
-    var action = rs.question('Enter the action{+,-,*,/,%}');
-
-    var int1 = toInt(fNum1);
-    var int2 = toInt(fNum2);
-}
-
-function toInt(number){
-    var num = ConvertBase.bin2dec(number);
-    console.log(num);
-}
 
 var ConvertBase = function (num) {
     return {
@@ -34,3 +16,30 @@ var ConvertBase = function (num) {
 ConvertBase.bin2dec = function (num) {
     return ConvertBase(num).from(2).to(10);
 };
+
+main();
+
+function main(){
+    var fNum1 = rs.question('1st Number: ');
+    var fNum2 = rs.question('2nd Number: ');
+    var action = rs.question('Enter the action{+,-,*,/,%}');
+
+    var int1 = toInt(fNum1);
+    var int2 = toInt(fNum2);
+    var operator = toOp(action, int1, int2);
+}
+
+function toInt(number){
+    var num = ConvertBase.bin2dec(number);
+    return num;
+}
+
+function toOp(operator, number1, number2){
+    var result;
+    switch(operator){
+        case "+":
+            result = number1 + number2;
+            break;
+    }
+    console.log(result);
+}
